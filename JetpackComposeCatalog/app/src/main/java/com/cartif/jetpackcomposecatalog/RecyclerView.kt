718cb8f8a413c1d03061ef57120245bc1ba2624c
CARTIF/dancal <dancal@cartif.es>
2023-05-26 13:45:55 +0200

    Comiezo con los RecyclerView

---
+++ JetpackComposeCatalog/app/src/main/java/com/cartif/jetpackcomposecatalog/RecyclerView.kt
@@ -0,0 +1,100 @@
+package com.cartif.jetpackcomposecatalog
+
+import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.*
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.cartif.jetpackcomposecatalog.model.Superhero
+
+@Composable
+fun SimpleRecyclerView() {
+    val myList = listOf("Aris", "Pepe", "MAnolo", "Jasime")
+
+    LazyColumn {
+        item { Text(text = "Header") }
+        items(myList) {
+            Text(text = "Hola me llamo $it")
+        }
+        item { Text(text = "Footer") }
+    }
+}
+
+@Composable
+fun SuperHeroView() {
+    val context = LocalContext.current
+    LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        items(getSuperheroes()) { superhero ->
+            ItemHero(superhero = superhero){
+                Toast.makeText(context, it.superheroName, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun SuperHeroGridView() {
+    val context = LocalContext.current
+    LazyVerticalGrid(cells = GridCells.Fixed(2), content = {
+        items(getSuperheroes()) { superhero ->
+            ItemHero(superhero = superhero)
+            { Toast.makeText(context, it.superheroName, Toast.LENGTH_SHORT).show() }
+        }
+    })
+}
+
+@Composable
+fun ItemHero(superhero: Superhero, onItemSelected:(Superhero)-> Unit){
+    Card(border = BorderStroke(2.dp, Color.Red), modifier = Modifier
+        .width(200.dp)
+        .clickable { onItemSelected(superhero) }) {
+        Column {
+            Image(
+                painter = painterResource(id = superhero.photo),
+                contentDescription = "superhero avatar",
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = superhero.superheroName,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = superhero.realName,
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                fontSize = 12.sp
+            )
+            Text(text = superhero.publisher, modifier = Modifier
+                .align(Alignment.End)
+                .padding(8.dp), fontSize = 10.sp)
+        }
+    }
+}
+
+fun getSuperheroes(): List<Superhero> {
+    return listOf(
+        Superhero("Spiderman", "Petter Parker", "Marvel", R.drawable.spiderman),
+        Superhero("Wolverine", "Logan", "Marvel", R.drawable.logan),
+        Superhero("Batman", "Bruce Wayne", "DC", R.drawable.batman),
+        Superhero("Thor", "Thor Odinson", "Marvel", R.drawable.thor),
+        Superhero("Flash", "Barry Allen", "DC", R.drawable.flash),
+        Superhero("Green Lantern", "Alan Scott", "DC", R.drawable.green_lantern),
+        Superhero("Wonder Woman", "Princess Diana", "DC", R.drawable.wonder_woman),
+
+        )
+}
