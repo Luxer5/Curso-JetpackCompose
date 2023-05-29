8d9a75a9a00b14cad70286d0cd5384a815915874
CARTIF/dancal <dancal@cartif.es>
2023-05-29 11:26:33 +0200

    Terminado Scaffold

--- JetpackComposeCatalog/app/src/main/java/com/cartif/jetpackcomposecatalog/Scaffold.kt
+++ JetpackComposeCatalog/app/src/main/java/com/cartif/jetpackcomposecatalog/Scaffold.kt
@@ -1,17 +1,13 @@
 package com.cartif.jetpackcomposecatalog
 
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.rememberScaffoldState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
@@ -25,29 +21,36 @@ fun ScaffoldExample() {
 
     Scaffold(
         topBar = {
-            MyTopAppBar {
+            MyTopAppBar(onClickIcon = {
                 coroutineScope.launch {
                     scaffoldState.snackbarHostState.showSnackbar(
                         "Has pulsado $it"
                     )
                 }
-            }
+            }, onClickDrawer = { coroutineScope.launch { scaffoldState.drawerState.open() } })
         },
-        scaffoldState = scaffoldState
+        scaffoldState = scaffoldState,
+        bottomBar = { MyBottomNavigation() },
+        floatingActionButton = { MyFAB() },
+        floatingActionButtonPosition = FabPosition.Center,
+        isFloatingActionButtonDocked = false,
+        drawerContent = { MyDrawer() { coroutineScope.launch { scaffoldState.drawerState.close() } } },
+        drawerGesturesEnabled = false
     ) {
 
     }
 }
 
 @Composable
-fun MyTopAppBar(onClickIcon: (String) -> Unit) {
+fun MyTopAppBar(onClickIcon: (String) -> Unit, onClickDrawer: () -> Unit) {
     TopAppBar(
         title = { Text(text = "Mi primera toolbar") },
         backgroundColor = Color.Red,
-        contentColor = Color.White, elevation = 4.dp,
+        contentColor = Color.White,
+        elevation = 4.dp,
         navigationIcon = {
-            IconButton(onClick = { onClickIcon("atras") }) {
-                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "back")
+            IconButton(onClick = { onClickDrawer() }) {
+                Icon(imageVector = Icons.Filled.Menu, contentDescription = "menu")
             }
         }, actions = {
             IconButton(onClick = { onClickIcon("buscar") }) {
@@ -58,4 +61,64 @@ fun MyTopAppBar(onClickIcon: (String) -> Unit) {
             }
         }
     )
+}
+
+@Composable
+fun MyBottomNavigation() {
+    var index by remember { mutableStateOf(0) }
+
+    BottomNavigation(backgroundColor = Color.Red, contentColor = Color.White) {
+        BottomNavigationItem(
+            selected = index == 0,
+            onClick = { index = 0 },
+            icon = { Icon(imageVector = Icons.Default.Home, contentDescription = "Home") },
+            label = { Text(text = "Home") })
+        BottomNavigationItem(
+            selected = index == 1,
+            onClick = { index = 1 },
+            icon = { Icon(imageVector = Icons.Default.Favorite, contentDescription = "Favorite") },
+            label = { Text(text = "Favorite") })
+        BottomNavigationItem(
+            selected = index == 2,
+            onClick = { index = 2 },
+            icon = { Icon(imageVector = Icons.Default.Person, contentDescription = "Person") },
+            label = { Text(text = "Person") })
+    }
+}
+
+@Composable
+fun MyFAB() {
+    FloatingActionButton(
+        onClick = { },
+        backgroundColor = Color.Yellow,
+        contentColor = Color.Black
+    ) {
+        Icon(imageVector = Icons.Filled.Add, contentDescription = "Add")
+    }
+}
+
+@Composable
+fun MyDrawer(onCloseDrawer: () -> Unit) {
+    Column(modifier = Modifier.padding(8.dp)) {
+        Text(
+            text = "Primera opcion", modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        )
+        Text(
+            text = "Segunda opcion", modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        )
+        Text(
+            text = "Tercera opcion", modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        )
+        Text(
+            text = "Cuarta opcion", modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        )
+    }
 }
