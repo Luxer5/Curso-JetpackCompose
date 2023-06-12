ace860c53971442c8e62a2f5dcd8ad35819ac451
CARTIF/dancal <dancal@cartif.es>
2023-06-12 07:19:01 +0200

    Comienzo de Navigation

--- JetpackComposeCatalog/app/src/main/java/com/cartif/jetpackcomposecatalog/MainActivity.kt
+++ JetpackComposeCatalog/app/src/main/java/com/cartif/jetpackcomposecatalog/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.cartif.jetpackcomposecatalog
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.expandVertically
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
@@ -19,17 +16,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.state.ToggleableState
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.cartif.jetpackcomposecatalog.model.Rutas
 import com.cartif.jetpackcomposecatalog.ui.theme.JetpackComposeCatalogTheme
-import org.intellij.lang.annotations.JdkConstants.CalendarMonth
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalFoundationApi::class)
@@ -66,8 +64,14 @@ class MainActivity : ComponentActivity() {
                         //SuperHeroGridView()
                         //SuperHeroWithEspecialControls()
                         //SuperHeroStickyView()
-                        ScaffoldExample()
+                        //ScaffoldExample()
 
+                    }
+                    val navigationController = rememberNavController()
+                    NavHost(navController = navigationController, startDestination = Rutas.Pantalla1.route ){
+                        composable(Rutas.Pantalla1.route){ Screen1(navigationController) }
+                        composable(Rutas.Pantalla2.route){ Screen2(navigationController) }
+                        composable(Rutas.Pantalla3.route){ Screen3(navigationController) }
                     }
                 }
             }
---
+++ JetpackComposeCatalog/app/src/main/java/com/cartif/jetpackcomposecatalog/Navigation.kt
@@ -0,0 +1,56 @@
+package com.cartif.jetpackcomposecatalog
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+import com.cartif.jetpackcomposecatalog.model.Rutas.*
+
+@Composable
+fun Screen1(navController: NavHostController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Cyan)
+    ) {
+        Text(text = "Pantalla 1", modifier = Modifier.align(Alignment.Center).clickable { navController.navigate(
+            Pantalla2.route) })
+    }
+}
+@Composable
+fun Screen2(navController: NavHostController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Green)
+    ) {
+        Text(text = "Pantalla 2", modifier = Modifier.align(Alignment.Center).clickable { navController.navigate(
+            Pantalla3.route) })
+    }
+}
+@Composable
+fun Screen3(navController: NavHostController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Magenta)
+    ) {
+        Text(text = "Pantalla 3", modifier = Modifier.align(Alignment.Center))
+    }
+}
+@Composable
+fun Screen4(navController: NavHostController, name: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Magenta)
+    ) {
+        Text(text = "Pantalla 3", modifier = Modifier.align(Alignment.Center))
+    }
+}
